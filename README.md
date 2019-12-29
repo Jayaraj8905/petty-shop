70b7401ca3d42dcdf0ea794b200da5ea41fcc396
Jayaraj <jayaraj8905@gmail.com>
2019-12-29 10:58:51 +0800

    Anything can start with nothing...

---
+++ README.md
@@ -0,0 +1,7 @@
+# petty_shop
+
+Shopping App for the petty shops based on Dart and Flutter
+
+## Getting Started
+
+For more Information please watch this page.....
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PettyShopApp());
+
+class PettyShopApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Petty Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.deepOrangeAccent
+      ),
+      home: Center(
+        child: Text('Anything can start with nothing'),
+      ),
+    );
+  }
+}
