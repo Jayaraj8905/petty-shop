821475da5797da4a57695a55466895269ba2de50
Jayaraj <jayaraj8905@gmail.com>
2019-12-31 16:37:57 +0800

    State management implemenatation
    Installed the providers package
    Implementation of state management
    Refactor -> Appending the Screen in file and class name for screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import './../models/product.dart';
-import './../DATA.dart';
+import 'package:provider/provider.dart';
+import './../providers/products.dart';
 
-class ProductDetail extends StatelessWidget {
+class ProductDetailScreen extends StatelessWidget {
 
   static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final String productId = ModalRoute.of(context).settings.arguments as String;
-    final Product product = PRODUCTS.firstWhere((prod) => prod.id == productId);
+    final product = Provider.of<Products>(context).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(
