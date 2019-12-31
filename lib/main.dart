9825fb4ec19dfc4f28c5d2b5cbd36ae49c636422
Jayaraj <jayaraj8905@gmail.com>
2019-12-31 18:41:26 +0800

    Clean up - Using provider.value instead of builder

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class PettyShopApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (context) => Products(),
+    return ChangeNotifierProvider.value(
+      value: Products(),
       child: MaterialApp(
         title: 'Petty Shop',
         theme: ThemeData(
