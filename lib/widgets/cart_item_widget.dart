77cd2da9bbf91652f9613ebe86cd9fb24ee250f4
Jayaraj <jayaraj8905@gmail.com>
2020-01-01 11:52:00 +0800

    Deleting cart item using dismissable

--- lib/widgets/cart_item_widget.dart
+++ lib/widgets/cart_item_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './../providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
 
@@ -18,28 +20,51 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 10,
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Theme.of(context).primaryIconTheme.color,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text('\$$price'),
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).toggleCart(
+          productId
+        );
+      },
+      direction: DismissDirection.endToStart,
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$$price'),
+                ),
               ),
             ),
+            title: Text(name),
+            subtitle: Text('Total: \$${price * unit_value}'),
+            trailing: Text('$unit_value x'),
           ),
-          title: Text(name),
-          subtitle: Text('Total: \$${price * unit_value}'),
-          trailing: Text('$unit_value x'),
         ),
       ),
     );
+    
   }
 
 }
