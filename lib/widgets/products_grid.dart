a4c9da5b6fc95bc43859cf3b9398315c27c0ee23
Jayaraj <jayaraj8905@gmail.com>
2020-01-02 21:49:49 +0800

    Clean up

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './../widgets/product_item.dart';
-import './../providers/product.dart';
 import './../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
