76cdd0341829a06ecbd0a7c3a7f6b930a12dc285
Jayaraj <jayaraj8905@gmail.com>
2020-01-04 17:09:12 +0800

    Listing the products in the order item

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './../providers/orders.dart';
@@ -14,20 +16,43 @@ class OrderItemWidget extends StatelessWidget {
         horizontal: 15,
         vertical: 10
       ),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: EdgeInsets.all(5),
-            child: FittedBox(
-              child: Text('\$${order.price}')
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$${order.price}')
+                ),
+              ),
+            ),
+            title: Text('Order No: ${order.id}'),
+            subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(order.createDate)),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more), onPressed: () {},
             ),
           ),
-        ),
-        title: Text('Order No: ${order.id}'),
-        subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(order.createDate)),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more), onPressed: () {},
-        ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            height: min(order.products.length * 20.0 + 10, 100),
+            child: ListView(
+              children: order.products.map((cartItem) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      cartItem.name
+                    ),
+                    Text(
+                      '\$${cartItem.price} x ${cartItem.unit_value}'
+                    )
+                  ],
+                );
+              }).toList()
+            ),
+          )
+        ],
       ),
     );
   }
