ee7f1d1385b3e3819e8e3fd47c7a794e05aa555d
Jayaraj <jayaraj8905@gmail.com>
2020-01-04 16:03:04 +0800

    Removed cart Id

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -3,14 +3,12 @@ import './cart.dart';
 
 class OrderItem {
   final int id;
-  final int cartId;
   final List<CartItem> products;
   final double price;
   final DateTime createDate;
 
   OrderItem({
     @required this.id,
-    @required this.cartId,
     @required this.products,
     @required this.price,
     @required this.createDate
@@ -25,11 +23,10 @@ class Orders with ChangeNotifier {
   }
 
   /// Add the order information
-  void addOrder(int cartId, List<CartItem> products, double price) {
+  void addOrder(List<CartItem> products, double price) {
     _orders.insert(0, 
       OrderItem(
         id: DateTime.now().millisecond,
-        cartId: cartId,
         products: products,
         price: price,
         createDate: DateTime.now()
