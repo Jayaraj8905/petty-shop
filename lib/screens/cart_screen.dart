485b9642c2e59d2ff1ce6d4c61761aa8d7fec443
Jayaraj <jayaraj8905@gmail.com>
2020-01-04 20:02:19 +0800

    Show snack bar after order

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -7,10 +7,13 @@ import './../widgets/cart_item_widget.dart';
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
 
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
     return Scaffold(
+      key: _scaffoldKey, // TODO: Temporary solution to show the snack bar, until the review order implemntation
       appBar: AppBar(
         title: Text('Cart'),
       ),
@@ -43,7 +46,12 @@ class CartScreen extends StatelessWidget {
                     onPressed: () {
                       Provider.of<Orders>(context, listen: false)
                       .addOrder(cart.items.values.toList(), cart.totalAmount);
-                      cart.clear();
+                        final snackBar = SnackBar(
+                          content: Text('Ordered!!!'),
+                          duration: Duration(seconds: 1),
+                        );
+                        _scaffoldKey.currentState.showSnackBar(snackBar);
+                        cart.clear();
                     },
                     textColor: Theme.of(context).primaryColor,
                   )
