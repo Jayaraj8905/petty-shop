b811eb2b4bd5f92b9bc0b1f43852dae367bff5ae
Jayaraj <jayaraj8905@gmail.com>
2020-01-04 19:21:45 +0800

    Dummy implementation for leaving the shop

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -2,14 +2,42 @@ import 'package:flutter/material.dart';
 import './../screens/orders_screen.dart';
 
 class AppDrawer extends StatelessWidget {
+
+  leaveShop(context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // title: Text('Implementation in Progress'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('You are about to leave the shop.'),
+                Text('Are you sure?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      semanticLabel: 'Ssdfs',
       child: Column(
         children: <Widget>[
           AppBar(
-            title: Text('Jade Minimart'),
+            title: Text('Hello Raj!'),
             automaticallyImplyLeading: false,
           ),
           ListTile(
@@ -24,6 +52,14 @@ class AppDrawer extends StatelessWidget {
             onTap: () => Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName),
           ),
           Divider(),
+          ListTile(
+            leading: Icon(Icons.arrow_left),
+            title: Text('Bye bye!!'),
+            onTap: () {
+              Navigator.of(context).pop();
+              leaveShop(context);
+            }
+          ),
         ],
       ),
     );
