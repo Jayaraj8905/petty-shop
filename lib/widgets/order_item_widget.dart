8e95fe4c75d3665c81c95dd8260f1d85bf62cd5e
Jayaraj <jayaraj8905@gmail.com>
2020-01-04 17:14:13 +0800

    Expandable feature for order products

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -4,11 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './../providers/orders.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
 
   OrderItemWidget(this.order);
 
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var _isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,38 +29,43 @@ class OrderItemWidget extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.all(5),
                 child: FittedBox(
-                  child: Text('\$${order.price}')
+                  child: Text('\$${widget.order.price}')
                 ),
               ),
             ),
-            title: Text('Order No: ${order.id}'),
-            subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(order.createDate)),
+            title: Text('Order No: ${widget.order.id}'),
+            subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(widget.order.createDate)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more), onPressed: () {},
+              icon: _isExpanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more), 
+              onPressed: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;  
+                });
+              },
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-            height: min(order.products.length * 20.0 + 10, 100),
-            child: ListView(
-              children: order.products.map((cartItem) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      cartItem.name
-                    ),
-                    Text(
-                      '\$${cartItem.price} x ${cartItem.unit_value}'
-                    )
-                  ],
-                );
-              }).toList()
-            ),
-          )
+          if (_isExpanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              child: ListView(
+                children: widget.order.products.map((cartItem) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        cartItem.name
+                      ),
+                      Text(
+                        '\$${cartItem.price} x ${cartItem.unit_value}'
+                      )
+                    ],
+                  );
+                }).toList()
+              ),
+            )
         ],
       ),
     );
   }
-
 }
