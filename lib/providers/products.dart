b4b741e27445bd7f890c2770d68e2d6f6ddc018a
Jayaraj <jayaraj8905@gmail.com>
2020-01-05 09:17:55 +0800

    Fetching the products from the backend

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,114 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import './product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    Product(
-      id: '1',
-      name: 'Onions',
-      description: 'About the onion',
-      price: 123.00,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/71rAIKxakYL._SX679_.jpg'
-    ),
-    Product(
-      id: '2',
-      name: 'Tomato',
-      description: 'About the tomato',
-      price: 43.00,
-      image: 'https://dam.farmjournal.com/m/16f06f5f2c6978c5/webimage-946FE331-C128-4F61-AC32684C11417AC3.png'
-    ),
-    Product(
-      id: '3',
-      name: 'Dhall',
-      description: 'Dhall',
-      price: 28.50,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/81f17QBywiL._SY550_.jpg'
-    ),
-    Product(
-      id: '4',
-      name: 'Ragi Flour',
-      description: 'About Ragi Flour',
-      price: 24.30,
-      image: 'http://timbaktu-organic.org/wp-content/uploads/2015/03/Malted-Ragi-Flour-600-600x600.jpg'
-    ),
-    Product(
-      id: '5',
-      name: 'Ladies Finger',
-      description: 'About Ragi Flour',
-      price: 12.30,
-      image: 'https://5.imimg.com/data5/BR/ND/MY-1768518/fresh-lady-finger-500x500.jpg'
-    ),
-    Product(
-      id: '7',
-      name: 'Cotton Candy',
-      description: 'About the onion',
-      price: 2.00,
-      image: 'https://odditymall.com/includes/content/move-over-cheese-balls-cotton-candy-balls-are-here-to-take-over-thumb.jpg'
-    ),
-    Product(
-      id: '8',
-      name: 'Dairy Milk',
-      description: 'About the tomato',
-      price: 15.00,
-      image: 'https://www.waangoo.com/content/images/thumbs/0012491_cadbury-dairy-milk_600.jpeg'
-    ),
-    Product(
-      id: '9',
-      name: 'Murukku',
-      description: 'Dhall',
-      price: 0.50,
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo7PdnetDJurTlKQFRmFzpuLXCKdT0oDd0fhOkA2RDGJQQe3ks&s'
-    ),
-    Product(
-      id: '10',
-      name: 'Banana',
-      description: 'About Ragi Flour',
-      price: 2,      
-      image: 'https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?w=600&quality=85'
-    ),
-    Product(
-      id: '11',
-      name: 'Hamam Soap',
-      description: 'About Ragi Flour',
-      price: 30.00,
-      image: 'https://www.waangoo.com/content/images/thumbs/0009971_hamam-soap-100gm_600.jpeg'
-    ),
-    Product(
-      id: '12',
-      name: 'Cinthol',
-      description: 'About the onion',
-      price: 40.00,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/81aVEokflXL._SL1500_.jpg'
-    ),
-    Product(
-      id: '13',
-      name: 'Brinjal',
-      description: 'About the tomato',
-      price: 43.00,
-      image: 'https://5.imimg.com/data5/JD/VD/PH/ANDROID-51655394/product-jpeg-500x500.jpg'
-    ),
-    Product(
-      id: '14',
-      name: 'Sugar',
-      description: 'Sugar',
-      price: 38.00,
-      image: 'http://worldwidegeneral.com/wp-content/uploads/2017/08/20161023-Udhsugar-417x561.jpg'
-    ),
-    Product(
-      id: '15',
-      name: 'Parachute',
-      description: 'About Ragi Flour',
-      price: 35.00,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/61D7y8hUGmL._SL1500_.jpg'
-    ),
-    Product(
-      id: '16',
-      name: 'Peanut',
-      description: 'About Ragi Flour',
-      price: 4.00,
-      image: 'https://3.imimg.com/data3/XS/RQ/MY-11974068/java-groundnut-500x500.jpg'
-    ),
+   
   ];
+
+  Future<void> fetchProducts() async {
+    final url = "https://petty-shop.firebaseio.com/products.json";
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      List<Product> loadedProducts = [];
+      extractedData.forEach((id, value) {
+        loadedProducts.add(Product(
+          id: id,
+          name: value["name"],
+          price: value["price"].toDouble(),
+          description: value["description"],
+          image: value["image"]
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+    } catch(e) {
+      throw(e);
+    }
+  }
 
   List<Product> get items {
     return [..._items];
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './../providers/cart.dart';
+import './../providers/products.dart';
 import './../widgets/products_grid.dart';
 import './../widgets/badge.dart';
 import './../widgets/app_drawer.dart';
 import './cart_screen.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    setState(() {
+      _isLoading = true;
+    });
+
+    Future.delayed(Duration.zero).then((_) => 
+      Provider.of<Products>(context).fetchProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      }).catchError((e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("An Error occurred!"),
+            content: Text("Something went wrong"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Okay!'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          )
+        );
+      })
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +59,7 @@ class ProductOverviewScreen extends StatelessWidget {
               return Badge(
                 child: IconButton(
                   icon: Icon(Icons.shopping_cart),
-                  onPressed: () => {
-                    Navigator.of(context).pushNamed(CartScreen.routeName)
-                  },
+                  onPressed: () => Navigator.of(context).pushNamed(CartScreen.routeName)
                 ),
                 value: cart.itemCount.toString(),
               );
@@ -31,9 +68,8 @@ class ProductOverviewScreen extends StatelessWidget {
           
         ],
       ),
-      body: ProductsGrid(),
+      body: _isLoading ? Center(child: CircularProgressIndicator()) : ProductsGrid(),
       drawer: AppDrawer(),
     );
   }
-
 }
