f18065814c2879d88c9e461e3f6c6e724e9b5b90
Jayaraj <jayaraj8905@gmail.com>
2020-01-05 11:22:43 +0800

    Order to the backend and order button loader

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import './cart.dart';
 
 class OrderItem {
-  final int id;
+  final String id;
   final List<CartItem> products;
   final double price;
   final DateTime createDate;
@@ -23,15 +25,34 @@ class Orders with ChangeNotifier {
   }
 
   /// Add the order information
-  void addOrder(List<CartItem> products, double price) {
-    _orders.insert(0, 
-      OrderItem(
-        id: DateTime.now().millisecondsSinceEpoch,
-        products: products,
-        price: price,
-        createDate: DateTime.now()
-      )
-    );
-    notifyListeners();
+  void addOrder(List<CartItem> products, double price) async {
+    final url = "https://petty-shop.firebaseio.com/orders.json";
+    final timestamp = DateTime.now();
+    try {
+      final response = await http.post(
+        url, 
+        body: json.encode({
+          "price": price,
+          "createDate": timestamp.toIso8601String(),
+          "products": products.map((cp) => {
+            "id": cp.id,
+            "name": cp.name,
+            "price": cp.price,
+            "unit_value": cp.unit_value
+          }).toList()
+        })
+      );
+      _orders.insert(0, 
+        OrderItem(
+          id: json.decode(response.body)["name"],
+          products: products,
+          price: price,
+          createDate: timestamp
+        )
+      );
+      notifyListeners();
+    } catch(e) {
+      throw(e);
+    }
   }
 }
