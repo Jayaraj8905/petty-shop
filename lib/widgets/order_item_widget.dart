01d392fea490aec6f38f84c7389e1af9e8e672cf
Jayaraj <jayaraj8905@gmail.com>
2020-03-15 10:43:40 +0800

    Order Item animation

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -17,35 +17,38 @@ class _OrderItemWidgetState extends State<OrderItemWidget> {
   var _isExpanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 10
-      ),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            onTap: () {
-              setState(() {
-                _isExpanded = !_isExpanded;  
-              });
-            },
-            leading: CircleAvatar(
-              child: Padding(
-                padding: EdgeInsets.all(5),
-                child: FittedBox(
-                  child: Text('\$${widget.order.price}')
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _isExpanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10
+        ),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              onTap: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;  
+                });
+              },
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: FittedBox(
+                    child: Text('\$${widget.order.price}')
+                  ),
                 ),
               ),
+              title: Text('Order No: ${widget.order.id}'),
+              subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(widget.order.createDate)),
+              trailing: _isExpanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more)
             ),
-            title: Text('Order No: ${widget.order.id}'),
-            subtitle: Text(DateFormat('dd MMM yyyy hh:mm').format(widget.order.createDate)),
-            trailing: _isExpanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more)
-          ),
-          if (_isExpanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
+              height: _isExpanded ? min(widget.order.products.length * 20.0 + 10, 100) : 0,
               child: ListView(
                 children: widget.order.products.map((cartItem) {
                   return Row(
@@ -62,7 +65,8 @@ class _OrderItemWidgetState extends State<OrderItemWidget> {
                 }).toList()
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
