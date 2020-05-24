100ccfc0cb32cd3582d88eaa6a536eb96187ed26
Jayaraj <jayaraj8905@gmail.com>
2020-05-24 11:48:32 +0800

    Shop Add screen with base form

--- lib/screens/shop_add_screen.dart
+++ lib/screens/shop_add_screen.dart
@@ -9,15 +9,66 @@ class ShopAddScreen extends StatefulWidget {
 }
 
 class _ShopAddScreenState extends State<ShopAddScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  void _onSave(BuildContext context) {
+    if (_formKey.currentState.validate()) {
+      print('Its valid');
+      // TODO: Save the data to firebase
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Shop has been Added'), 
+          duration: Duration(seconds: 2)
+        ));
+    } else {
+      
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Add your shop'),
+        title: Text('Add your shop')
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('Add shop form')
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Shop Name'
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Invalid shop name';
+                          }
+                          return null;
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Save'),
+                        onPressed: () => _onSave(context),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Theme.of(context).primaryTextTheme.button.color,
+                        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
