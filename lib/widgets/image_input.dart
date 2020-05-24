539be7ed47426bb027ca0e646de91bfee8904bfc
Jayaraj <jayaraj8905@gmail.com>
2020-05-24 12:08:58 +0800

    Image input widget base

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  ImageInput({Key key}) : super(key: key);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('Image will come here'),
+    );
+  }
+}
