ba2100063c31bd7925f9d53f481b3ce994bab412
Jayaraj <jayaraj8905@gmail.com>
2020-05-24 14:04:24 +0800

    Image picker container

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -9,9 +9,31 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   @override
+
+  void _selectImage() {
+    
+  }
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('Image will come here'),
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey)
+          ),
+          child: Text('No image taken'),
+          alignment: Alignment.center,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            onPressed: _selectImage, 
+            icon: Icon(Icons.add_a_photo),
+            label: Text('Take/Select Picture'),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
     );
   }
 }
