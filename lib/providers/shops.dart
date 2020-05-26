b5a0fc97d151721c4d249721f60242fd5824b3a6
Jayaraj <jayaraj8905@gmail.com>
2020-05-26 14:40:52 +0800

    Base provider to add shops

---
+++ lib/providers/shops.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Shops with ChangeNotifier {
+  List<Shop> _list = [];
+
+  void addShop(String name, File image) {
+    var id = _list.length.toString();
+    _list.add(new Shop(id: id, name: name, image: image));
+    notifyListeners();
+  }
+
+  List<Shop> fetchShops() {
+    return this._list;
+  }
+}
+
+class Shop {
+  final String id;
+  final String name;
+  final File image;
+  
+  Shop({
+    @required this.id,
+    @required this.name,
+    @required this.image
+  });
+}
