4e7e86f7b08f842cd526c6f82dc312139ca652ac
Jayaraj <jayaraj8905@gmail.com>
2020-06-02 17:57:46 +0800

    Converting location field to form field

--- lib/widgets/location_input_field.dart
+++ lib/widgets/location_input_field.dart
@@ -4,78 +4,75 @@ import 'package:petty_shop/models/location.dart';
 import 'package:petty_shop/screens/map_screen.dart';
 import './../helpers/location_helper.dart';
 
-class LocationInputField extends StatefulWidget {
-  LocationInputField({Key key}) : super(key: key);
+class LocationInputField extends FormField<LocationDetails> {
+  LocationInputField({
+    FormFieldSetter<LocationDetails> onSaved,
+    FormFieldValidator<LocationDetails> validator,
+    LocationDetails initialValue,
+    bool autovalidate = false
+  }): super(
+    onSaved: onSaved,
+    validator: validator,
+    initialValue: initialValue,
+    autovalidate: autovalidate,
+    builder: (FormFieldState<LocationDetails> state) {
+      Future<void> _getCurrentLocation() async {
+        final locationData = await Location().getLocation();
+        final locationDetails = LocationDetails(latitude: locationData.latitude, longitude: locationData.longitude);
+        state.didChange(locationDetails);
+      } 
 
-  @override
-  _LocationInputFieldState createState() => _LocationInputFieldState();
-}
-
-class _LocationInputFieldState extends State<LocationInputField> {
-  String _locationUrl;
-  Future<void> _getCurrentLocation() async {
-    final locationData = await Location().getLocation();
-    final staticUrl = LocationHelper.generateLocationPreviewImage(latitude: locationData.latitude, longitude: locationData.longitude);
-    setState(() {
-      _locationUrl = staticUrl;
-    });
-  } 
-
-  Future<void> _pickLocation() async {
-    final currentLocation = await Location().getLocation();
-    final locationDetails = LocationDetails(latitude: currentLocation.latitude, longitude: currentLocation.longitude);
-    final locationData = await Navigator.of(context).push(
-      MaterialPageRoute(
-        fullscreenDialog: true,
-        builder: (ctx) => MapScreen(locationDetails: locationDetails,preview: true)
-      )
-    );
-    final staticUrl = LocationHelper.generateLocationPreviewImage(latitude: locationData.latitude, longitude: locationData.longitude);
-    setState(() {
-      _locationUrl = staticUrl;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: double.infinity,
-          height: 200,
-          decoration: BoxDecoration(
-            border: Border.all(width: 1, color: Colors.grey)
-          ),
-          alignment: Alignment.center,
-          child: _locationUrl == null
+      Future<void> _pickLocation() async {
+        final currentLocation = await Location().getLocation();
+        final defLocation = LocationDetails(latitude: currentLocation.latitude, longitude: currentLocation.longitude);
+        final pickedLocation = await Navigator.of(state.context).push(
+          MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (ctx) => MapScreen(locationDetails: defLocation,preview: true)
+          )
+        );
+        final locationDetails = LocationDetails(latitude: pickedLocation.latitude, longitude: pickedLocation.longitude);
+        state.didChange(locationDetails);
+      }
+      return Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.grey)
+            ),
+            alignment: Alignment.center,
+            child: state.value == null
                   ? Text(
                       'No location chosen',
                       textAlign: TextAlign.center,
                     )
                   : Image.network(
-                    _locationUrl,
+                    LocationHelper.generateLocationPreviewImage(latitude: state.value.latitude, longitude: state.value.longitude),
                     fit: BoxFit.cover,
                     width: double.infinity,
                   ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton.icon(
-              onPressed: _getCurrentLocation, 
-              icon: Icon(Icons.location_on), 
-              label: Text('Current Location'),
-              textColor: Theme.of(context).primaryColor,
-            ),
-            FlatButton.icon(
-              onPressed: _pickLocation, 
-              icon: Icon(Icons.map), 
-              label: Text('Select on Map'),
-              textColor: Theme.of(context).primaryColor,
-            )
-          ],
-        )
-      ],
-    );
-  }
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed: _getCurrentLocation, 
+                icon: Icon(Icons.location_on), 
+                label: Text('Current Location'),
+                textColor: Theme.of(state.context).primaryColor,
+              ),
+              FlatButton.icon(
+                onPressed: _pickLocation, 
+                icon: Icon(Icons.map), 
+                label: Text('Select on Map'),
+                textColor: Theme.of(state.context).primaryColor,
+              )
+            ],
+          ),
+        ],
+      );
+    }
+  );
 }
